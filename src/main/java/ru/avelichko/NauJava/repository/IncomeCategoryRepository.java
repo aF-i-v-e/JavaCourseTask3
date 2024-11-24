fd10bb7e84827da681ba15cbc52bc82f8b1580ff
aF-i-v-e <81624258+aF-i-v-e@users.noreply.github.com>
2024-11-24 16:56:46 +0500

    Добавление категории доходов и отображение списка категории доходов (#5)

--- src/main/java/ru/avelichko/NauJava/repository/IncomeCategoryRepository.java
+++ src/main/java/ru/avelichko/NauJava/repository/IncomeCategoryRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.avelichko.NauJava.model.IncomeCategory;
 
+import java.util.List;
+
 @RepositoryRestResource(path = "income_categories")
 public interface IncomeCategoryRepository extends CrudRepository<IncomeCategory, Long> {
-
+    List<IncomeCategory> findByIncomeCategoryName(String incomeCategoryName);
 }
--- src/main/java/ru/avelichko/NauJava/webController/CategoryController.java
+++ src/main/java/ru/avelichko/NauJava/webController/CategoryController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.avelichko.NauJava.exception.ServiceException;
 import ru.avelichko.NauJava.model.ExpenseCategory;
+import ru.avelichko.NauJava.model.IncomeCategory;
 import ru.avelichko.NauJava.service.ExpenseCategoryService;
+import ru.avelichko.NauJava.service.IncomeCategoryService;
 
 import java.util.List;
 
@@ -18,10 +20,15 @@ public class CategoryController {
     @Autowired
     private ExpenseCategoryService expenseCategoryService;
 
+    @Autowired
+    private IncomeCategoryService incomeCategoryService;
+
     @RequestMapping("/category/list")
     public String getCategoryList(Model model) {
         List<ExpenseCategory> expenseCategories = expenseCategoryService.getExpenseCategories();
+        List<IncomeCategory> incomeCategories = incomeCategoryService.getIncomeCategories();
         model.addAttribute("expenseCategories", expenseCategories);
+        model.addAttribute("incomeCategories", incomeCategories);
         return "categories-list";
     }
 
@@ -38,6 +45,22 @@ public class CategoryController {
     @RequestMapping("/category/expenses/delete/{id}")
     public String deleteExpenseCategory(@PathVariable("id") long id, Model model) {
         expenseCategoryService.deleteExpenseCategoryById(id);
+        return getCategoryList(model);
+    }
+
+    @RequestMapping("/category/incomes/delete/{id}")
+    public String deleteIncomeCategory(@PathVariable("id") long id, Model model) {
+        incomeCategoryService.deleteIncomeCategoryById(id);
+        return getCategoryList(model);
+    }
+
+    @RequestMapping("/category/incomes/add")
+    public String addIncomeCategory(@Valid IncomeCategory incomeCategory, Model model) {
+        try {
+            incomeCategoryService.addIncomeCategory(incomeCategory);
+        } catch (ServiceException exception) {
+            model.addAttribute("incomeCategoryMessage", exception.getMessage());
+        }
         return getCategoryList(model);
     }
 }
