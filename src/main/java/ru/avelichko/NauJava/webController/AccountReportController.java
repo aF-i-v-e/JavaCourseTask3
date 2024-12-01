074cfb93ae544c6f3802ad262ed9675ef3c04748
aF-i-v-e <avelichko.ru@yandex.ru>
2024-12-01 19:49:38 +0500

    6.4. Формирование отчёта за заданный период времени по конкретной категории расходов.

--- src/main/java/ru/avelichko/NauJava/webController/AccountReportController.java
+++ src/main/java/ru/avelichko/NauJava/webController/AccountReportController.java
@@ -10,8 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.avelichko.NauJava.model.Account;
 import ru.avelichko.NauJava.model.AccountReport;
+import ru.avelichko.NauJava.model.ExpenseCategory;
 import ru.avelichko.NauJava.service.AccountReportService;
 import ru.avelichko.NauJava.service.AccountService;
+import ru.avelichko.NauJava.service.ExpenseCategoryService;
+
+import java.util.List;
 
 @Controller
 public class AccountReportController {
@@ -20,6 +24,9 @@ public class AccountReportController {
 
     @Autowired
     private AccountReportService accountReportService;
+
+    @Autowired
+    private ExpenseCategoryService expenseCategoryService;
 
     private final String successReport = "Отчёт успешно сформирован!";
     private final String notSuccessReport = "Ошибка формирования отчёта!";
@@ -43,7 +50,22 @@ public class AccountReportController {
     }
 
     @GetMapping("/report/expense/specific")
-    public String getSpecificExpenseReport() {
-        return "";
+    public String getSpecificExpenseReport(Model model) {
+        List<ExpenseCategory> expenseCategories = expenseCategoryService.getExpenseCategories();
+        model.addAttribute("expenseCategories", expenseCategories);
+        return "report-expense-specific";
+    }
+
+    @RequestMapping("/report/expense/specific/create")
+    public String createSpecificExpenseReport(@Valid ExpenseCategory expenseCategory, @Valid AccountReport accountReport, Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Account account = accountService.getAccountByLogin(auth.getName());
+
+        Boolean isSuccess = accountReportService.fillSpecificExpenseReport(account, accountReport, expenseCategory);
+        String message = isSuccess ? successReport : notSuccessReport;
+
+        model.addAttribute("message", message);
+        model.addAttribute("generatedReport", accountReport);
+        return getSpecificExpenseReport(model);
     }
 }
