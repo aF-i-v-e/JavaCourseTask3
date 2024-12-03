c54da09f55af2c5686fc743d550715f8e253bb39
aF-i-v-e <avelichko.ru@yandex.ru>
2024-12-03 22:04:27 +0500

    Рефакторинг: сохранение отчета только если он не существовал до этого в бд.

--- src/main/java/ru/avelichko/NauJava/repository/AccountReportRepository.java
+++ src/main/java/ru/avelichko/NauJava/repository/AccountReportRepository.java
@@ -2,8 +2,12 @@ package ru.avelichko.NauJava.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import ru.avelichko.NauJava.model.Account;
 import ru.avelichko.NauJava.model.AccountReport;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "account_reports")
 public interface AccountReportRepository extends CrudRepository<AccountReport, Long> {
+    List<AccountReport> findByAccount(Account account);
 }
